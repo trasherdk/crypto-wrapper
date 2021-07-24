d50919ed5d6436c1316bb2d7181beac990a804e0
Muhammad Fauzan <fncolon@pm.me>
2021-07-24 23:11:10 +0700

    fix: unhandled promise rejection #29

--- src/lib/3des.js
+++ src/lib/3des.js
@@ -1,23 +1,31 @@
 const crypto = require("crypto");
 
 module.exports.encrypt = async (data, key) => {
-  const derivedKey = crypto.pbkdf2Sync(key, 'salt', 10000, 12, 'sha1');
+  const derivedKey = crypto.pbkdf2Sync(key, "salt", 10000, 12, "sha1");
 
-  const cipher = crypto.createCipheriv('des-ede3', derivedKey.toString('hex'), '');
+  const cipher = crypto.createCipheriv(
+    "des-ede3",
+    derivedKey.toString("hex"),
+    ""
+  );
 
-  let encrypted = cipher.update(data, 'utf8', 'base64');
-  encrypted += cipher.final('base64');
+  let encrypted = cipher.update(data, "utf8", "base64");
+  encrypted += cipher.final("base64");
 
   return encrypted;
-}
+};
 
 module.exports.decrypt = async (data, key) => {
-  const derivedKey = crypto.pbkdf2Sync(key, 'salt', 10000, 12, 'sha1');
+  const derivedKey = crypto.pbkdf2Sync(key, "salt", 10000, 12, "sha1");
 
-  const decipher = crypto.createDecipheriv('des-ede3', derivedKey.toString('hex'), '');
+  const decipher = crypto.createDecipheriv(
+    "des-ede3",
+    derivedKey.toString("hex"),
+    ""
+  );
 
-  let decrypted = decipher.update(data, 'base64', 'utf8');
-  decrypted += decipher.final('utf8');
+  let decrypted = decipher.update(data, "base64", "utf8");
+  decrypted += decipher.final("utf8");
 
   return decrypted;
-}
+};
--- tests/3des.test.js
+++ tests/3des.test.js
@@ -1,12 +1,12 @@
 const { tripleDes } = require("../src");
 
-let encrypted = tripleDes.encrypt("test","encryptlab");
-let decrypted = tripleDes.decrypt("A6fH4bsKKgw=","encryptlab");
+let encrypted = tripleDes.encrypt("test", "encryptlab");
+let decrypted = tripleDes.decrypt("iS22vsedn7c=", "encryptlab");
 
-test('is encrypt work?', () => {
+test("is encrypt work?", () => {
   expect(encrypted).toBeTruthy();
 });
 
-test('is decrypt work?', () => {
+test("is decrypt work?", () => {
   expect(decrypted).toBeTruthy();
 });
