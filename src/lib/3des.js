51f428d915630746f50d790148fd5f1fe027fa5b
TrasherDK <trasherdk@fumlersoft.dk>
2021-07-24 14:05:38 +0200

    test: fix 3des test & `src/lib/3des.js` - add `test:3des` to `"scripts"`

--- src/lib/3des.js
+++ src/lib/3des.js
@@ -1,23 +1,53 @@
 const crypto = require("crypto");
 
 module.exports.encrypt = async (data, key) => {
-  const derivedKey = crypto.pbkdf2Sync(key, 'salt', 10000, 12, 'sha1');
+  let encrypted = ""
+  let derivedKey
+  let cipher
 
-  const cipher = crypto.createCipheriv('des-ede3', derivedKey.toString('hex'), '');
+  try {
+    derivedKey = crypto.pbkdf2Sync(key, 'salt', 10000, 12, 'sha1');
+  }
+  catch (err) {
+    // console.error("Encrypt:", err)
+    return err
+  }
+  try {
+    cipher = crypto.createCipheriv('des-ede3', derivedKey.toString('hex'), '');
 
-  let encrypted = cipher.update(data, 'utf8', 'base64');
-  encrypted += cipher.final('base64');
+    encrypted = cipher.update(data, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+  }
+  catch (err) {
+    // console.error("Encrypt:", err)
+    return err
+  }
 
   return encrypted;
 }
 
 module.exports.decrypt = async (data, key) => {
-  const derivedKey = crypto.pbkdf2Sync(key, 'salt', 10000, 12, 'sha1');
+  let decrypted = ""
+  let derivedKey
+  let decipher
 
-  const decipher = crypto.createDecipheriv('des-ede3', derivedKey.toString('hex'), '');
+  try {
+    derivedKey = crypto.pbkdf2Sync(key, 'salt', 10000, 12, 'sha1');
+  }
+  catch (err) {
+    // console.error("Decrypt:", err)
+    return false
+  }
+  try {
+    decipher = crypto.createDecipheriv('des-ede3', derivedKey.toString('hex'), '');
 
-  let decrypted = decipher.update(data, 'base64', 'utf8');
-  decrypted += decipher.final('utf8');
+    decrypted = decipher.update(data, 'base64', 'utf8');
+    decrypted += decipher.final('utf8');
+  }
+  catch (err) {
+    // console.error("Decrypt:", err)
+    return err
+  }
 
   return decrypted;
 }
--- tests/3des.test.js
+++ tests/3des.test.js
@@ -1,12 +1,24 @@
 const { tripleDes } = require("../src");
 
-let encrypted = tripleDes.encrypt("test","encryptlab")
-let decrypted = tripleDes.decrypt(Buffer.from("A6fH4bsKKgw=","utf-8"),"encryptlab");
+const clearText = "test"
+const cryptText = "iS22vsedn7c=" // Just wrong "A6fH4bsKKgw="
+const password = "encryptlab"
 
 test('is encrypt work?', async () => {
+  const encrypted = tripleDes.encrypt(clearText, password)
+
   expect(await encrypted).toBeTruthy();
 });
 
 test('is decrypt work?', async () => {
+  const decrypted = tripleDes.decrypt(Buffer.from(cryptText, "base64"), password);
+
   expect(await decrypted).toBeTruthy();
 });
+
+test('is encrypt/decrypt what we expect?', async () => {
+  let enc = await tripleDes.encrypt(clearText, password)
+  let dec = await tripleDes.decrypt(Buffer.from(enc, "base64"), password);
+  expect(enc).toEqual(cryptText)
+  expect(dec).toEqual(clearText)
+})
