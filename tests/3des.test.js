2ec07dc67cac2ad7a61d338c813dc1aed37f1125
Muhammad Fauzan <fncolon@pm.me>
2021-07-24 23:12:10 +0700

    fix: unhandled promise rejection #29

--- tests/3des.test.js
+++ tests/3des.test.js
@@ -2,6 +2,7 @@ const { tripleDes } = require("../src");
 
 let encrypted = tripleDes.encrypt("test", "encryptlab");
 let decrypted = tripleDes.decrypt("iS22vsedn7c=", "encryptlab");
+console.log(decrypted);
 
 test("is encrypt work?", () => {
   expect(encrypted).toBeTruthy();
