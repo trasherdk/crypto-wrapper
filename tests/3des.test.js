d9b68bacf13134778096d7178741687fcbc39522
TrasherDK <trasherdk@fumlersoft.dk>
2021-07-24 12:43:35 +0200

    test: 3des

--- tests/3des.test.js
+++ tests/3des.test.js
@@ -1,12 +1,12 @@
 const { tripleDes } = require("../src");
 
-let encrypted = tripleDes.encrypt("test","encryptlab");
-let decrypted = tripleDes.decrypt("A6fH4bsKKgw=","encryptlab");
+let encrypted = tripleDes.encrypt("test","encryptlab")
+let decrypted = tripleDes.decrypt(Buffer.from("A6fH4bsKKgw=","utf-8"),"encryptlab");
 
-test('is encrypt work?', () => {
-  expect(encrypted).toBeTruthy();
+test('is encrypt work?', async () => {
+  expect(await encrypted).toBeTruthy();
 });
 
-test('is decrypt work?', () => {
-  expect(decrypted).toBeTruthy();
+test('is decrypt work?', async () => {
+  expect(await decrypted).toBeTruthy();
 });
